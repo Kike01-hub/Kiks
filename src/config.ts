2e5cafffb37fb51b4c05670b5dc61119edff6dd8
Kike01-hub <kikelomokukoyi@gmail.com>
2024-11-26 12:33:57 +0100

    Update config.ts
    Meta Title And Meta Description Updated

--- src/config.ts
+++ src/config.ts
@@ -2,6 +2,6 @@
 // You can import this data from anywhere in your site by using the `import` keyword.
 
 export const SITE_TITLE = 'Kukoyi Tawakalitu B. | Digital Marketing Specialist';
-export const SITE_DESCRIPTION = 'Experienced digital marketer skilled in SEO and social media manager, specialized in crafting strategies to boost brand visibility and drive business growth.';
+export const SITE_DESCRIPTION = 'Experienced digital marketer skilled in SEO and social media management, specialized in crafting strategies to boost brand visibility and drive business growth.';
 export const GENERATE_SLUG_FROM_TITLE = true
 export const TRANSITION_API = true
